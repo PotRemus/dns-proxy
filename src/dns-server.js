fa4e82393aa475a1a3090bb59b875c235d393f76
Remi MARGUERIT <R-MARGUERIT@bruneau.fr>
2019-12-04 11:27:45 +0100

    favorite server dns by domain

--- src/dns-server.js
+++ src/dns-server.js
@@ -2,11 +2,13 @@
 
 const dns = require('native-dns');
 const path = require('path');
-var fs = require('fs');
-var logger = require('./logger');
+const fs = require('fs');
+const async = require('async');
+const logger = require('./logger');
 
 class DsnServer {
   constructor(servers) {
+    this._favoriteDns = {};
     this._servers = servers;
     this._server = dns.createServer();
     this._server.on('listening', () => {
@@ -21,7 +23,9 @@ class DsnServer {
     this._server.on('socketError', (err, socket) => {
       logger.error(err);
     });
-    this._server.on('request', this._handleRequest);
+    this._server.on('request', (req, res) => {
+      return this._handleRequest(req, res);
+    });
   }
 
   start() {
@@ -35,26 +39,53 @@ class DsnServer {
     // proxy all questions
     // since proxying is asynchronous, store all callbacks
     request.question.forEach(question => {
-      f.push(cb => this._proxy(question, response, cb));
+      f.push(cb => {
+        return this._proxy(question, response, cb)
+      });
     });
     // do the proxying in parallel
     // when done, respond to the request by sending the response
-    parallel(f, function () { response.send(); });
+    async.parallel(f, function () { response.send(); });
   }
 
   async _proxy(question, response, cb) {
     console.log('proxying', question.name);
 
-    for (var i in this._servers) {
-      var dsnServer = this._servers[i];
-      let isSucces = await this._sendRequest(dsnServer, question, response);
-      if (isSucces) {
-        break;
-      } else {
-        console.error(`Error dns resolver (name: ${question.name}, server: ${dsnServer.address})`);
+    let isSucces = false;
+    if (this._favoriteDns[question.name]) {
+      let favoriteDns = this._favoriteDns[question.name];
+      if (favoriteDns && favoriteDns.expire > new Date()) {
+        let dsnServer = this._servers.find(server => {
+          return server.address === favoriteDns.address
+        });
+        if (dsnServer) {
+          isSucces = await this._sendRequest(dsnServer, question, response);
+        }
+      }
+    }
+
+    if (!isSucces) {
+      for (let i in this._servers) {
+        let dsnServer = this._servers[i];
+        isSucces = await this._sendRequest(dsnServer, question, response);
+        if (isSucces) {
+          this._favoriteDns[question.name] = {
+            expire: this._addDays(new Date(), 1),
+            address: dsnServer.address
+          }
+          break;
+        } else {
+          console.error(`Error dns resolver (name: ${question.name}, server: ${dsnServer.address})`);
+        }
       }
     }
     cb();
+  }
+
+  _addDays(date, days) {
+    let result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
   }
 
   _sendRequest(server, question, response) {
@@ -80,5 +111,5 @@ class DsnServer {
 }
 
 let settings = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'settings.json')));
-var dsnServer = new DsnServer(settings.servers);
+let dsnServer = new DsnServer(settings.servers);
 dsnServer.start();
